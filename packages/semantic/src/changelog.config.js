1916b23df3de98587506b14bb2df36c07799054e
Jerome Fitzgerald <jeromes@gmail.com>
2021-04-18 12:06:58 -0400

    💥 (semantic) bump to 3.0.0 ii
    BREAKING CHANGE: 💥️ Need to bump @jeromefitz/semantic eventually to 3.0.0 to reset

--- packages/semantic/src/changelog.config.js
+++ packages/semantic/src/changelog.config.js
@@ -1,5 +1,3 @@
-// const gitmojis = require('@jeromefitz/git-cz/data/gitmoji/index.json')
-
 const format = '{emoji}{scope} {branchName}{subject}'
 const questions = ['branchPrefix', 'breaking', 'type', 'scope', 'subject', 'body']
 const scopes = ['', 'codestyle', 'release', 'semantic']
