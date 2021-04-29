6fdcef350cb972ce9efa7cda46f326d7a33cf867
Jerome Fitzgerald <jeromes@gmail.com>
2021-04-29 18:33:52 -0400

    ✨ (codestyle) eslint + prettier variations for: react, react-native

--- packages/codestyle/.prettierrc.js
+++ packages/codestyle/.prettierrc.js
@@ -1,9 +1,3 @@
 module.exports = {
-  arrowParens: 'always',
-  endOfLine: 'auto',
-  printWidth: 85,
-  semi: false,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'es5',
+  ...require('./.prettierrc.react.js'),
 }
---
+++ packages/codestyle/.prettierrc.react-native.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  printWidth: 85,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+}
