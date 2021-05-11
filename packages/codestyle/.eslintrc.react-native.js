933d14aa9659692d82a65d75ea93bbd6bffe5061
Jerome Fitzgerald <jeromes@gmail.com>
2021-05-10 21:32:16 -0400

    ✨  (codestyle) import sort for eslint

--- packages/codestyle/.eslintrc.react-native.js
+++ packages/codestyle/.eslintrc.react-native.js
@@ -1,14 +1,17 @@
 module.exports = {
-  extends: ['prettier'],
+  extends: ['prettier', 'plugin:import/errors', 'plugin:import/warnings'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
-      plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+      plugins: ['@typescript-eslint', 'react', 'react-hooks', 'import'],
       extends: [
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'prettier',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
       ],
       parser: '@typescript-eslint/parser',
       parserOptions: {
@@ -38,11 +41,30 @@ module.exports = {
     },
   ],
   parser: '@babel/eslint-parser',
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', 'import'],
   // 0 off, 1 warn, 2 error
   rules: {
     complexity: [2, 10],
     'handle-callback-err': 2,
+    'import/default': 0, // @todo
+    'import/named': 0, // @todo
+    'import/namespace': 0, // @todo
+    'import/no-unresolved': 0, // @todo
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: '~**/**',
+            group: 'sibling',
+            position: 'before',
+          },
+        ],
+      },
+    ],
     'max-len': [
       2,
       {
