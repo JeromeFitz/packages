f0c12b4c6f8453d0217a1db914307cd3288479f3
Jerome Fitzgerald <jeromes@gmail.com>
2021-06-15 19:50:56 -0400

    ✅  (git-cz) customization for running tests w/ changelog

---
+++ packages/git-cz/changelog.config.js
@@ -0,0 +1,50 @@
+// @note(typescript) need to grab the built version for testing
+const isCI = require('is-ci')
+
+!isCI && require('dotenv').config({ path: './.env' })
+const config = require('./dist/themes/gitmoji').default
+
+const OVERRIDE_TEST = false
+
+const enabled = (OVERRIDE_TEST && (process.env.GIT_CZ || !OVERRIDE_TEST)) || true
+
+const typesCustom = {
+  override: {
+    branch: false,
+    code: ':sunrise_over_mountains:',
+    commit: 'override',
+    description: 'Custom type => override.',
+    emoji: '🌄️',
+    entity: '&#1F304;',
+    name: 'override',
+    section: 'Custom type => override.',
+    semver: null,
+  },
+}
+
+const commit = OVERRIDE_TEST
+  ? {
+      after: { ...config.commit.after, scope: '): ' },
+      before: { ...config.commit.before, scope: '(' },
+      format: '{type}{scope}{subject}',
+      maxMessageLength: 64,
+      minMessageLength: 3,
+      questions: ['commitScopes', 'commitTypes', 'commitSubject'],
+      scopes: ['', 'app'],
+    }
+  : config.commit
+
+const branch = OVERRIDE_TEST
+  ? {
+      format: '{branchType}{branchName}',
+      prefix: 'ABC-',
+      questions: ['branchFlag', 'branchTypes', 'branchName'],
+      url: 'https://abc.atlassian.net/browse/',
+    }
+  : config.branch
+
+const types = OVERRIDE_TEST ? { ...config.types, ...typesCustom } : config.types
+
+const changelog = { ...config, branch, commit, enabled, types }
+
+module.exports = changelog
