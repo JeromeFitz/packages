d16c3db114d037c377cd022baf50e9bccc3e5db8
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-01-14 00:40:43 -0500

    🩹  (notion) getTitle return; more exports [build]

--- packages/notion/src/utils/getTitle.ts
+++ packages/notion/src/utils/getTitle.ts
@@ -2,12 +2,15 @@ import _last from 'lodash/last.js'
 
 const getTitle = (title: string) => {
   const _title: string = _last(title.split('_')) ?? ''
+
   if (_title.length > 0) {
     _title
       .replace('Past', 'Cast Emeritus')
       .replace('Musical', 'Musical Director')
       .replace('Technical', 'Technical Director')
   }
+
+  return _title
 }
 
 export default getTitle
