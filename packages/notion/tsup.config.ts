b71bec805e5b6510c0702f47dd14f8540d9616d8
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-01-15 15:28:25 -0500

    🩹  (notion) minify build [build]

--- packages/notion/tsup.config.ts
+++ packages/notion/tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   dts: true,
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
-  minify: false,
+  minify: true,
   outDir: 'dist',
   sourcemap: false,
   splitting: false,
