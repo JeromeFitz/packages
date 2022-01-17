26e61823946ef40561841057057142afb4f2dc4e
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-01-17 11:00:08 -0500

    🚑️  (spotify) fix top tracks ♻️ code cleanup [build] (#340)
    - 🚑️ Fix data sent to `getArtistsGenres` and remove duplicate genres in return.
    - ♻️ Utilize `constants` bit more, and code cleanup
    - 🤣️ `@todo(types)` updates
    - 🖼️`getImages` do not instantiate `new Slugger()` as in this iteration we do not care about slug clashes.

--- packages/spotify/src/utils/getImage/index.ts
+++ packages/spotify/src/utils/getImage/index.ts
@@ -1,10 +1,21 @@
 import Slugger from 'github-slugger'
 import { getPlaiceholder } from 'plaiceholder'
 
+/**
+ * @hack(!cache)
+ *
+ * - if two images (tracks)
+ * - are the same (album),
+ * - we _do_ want same slug
+ *
+ */
+// const slugger = new Slugger()
+
 const getImage = async (url: string) => {
-  const slugger = new Slugger()
-  const slug = slugger.slug(url)
+  // const slug = slugger.slug(url)
+  const slug = Slugger.slug(url)
   const { base64, img } = await getPlaiceholder(url)
+
   return {
     base64,
     // @hack(ILoadImageImg)
