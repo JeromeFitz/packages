dea743ad190b6461e7af66b73187034ac3251a10
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-01-19 19:14:22 -0500

    👷  (packages) turbo cache for lint + build 🔺️

--- packages/semantic-config/tsup.config.ts
+++ packages/semantic-config/tsup.config.ts
@@ -1,1 +1,14 @@
-export { default } from '../../tsup.config'
+// import isCI from 'is-ci'
+import { defineConfig } from 'tsup'
+
+import { config as _config } from '../../tsup.config'
+
+const entry = ['src/*']
+const config = {
+  ..._config,
+  entry,
+}
+
+export default defineConfig({
+  ...config,
+})
