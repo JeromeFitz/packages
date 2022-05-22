923f6d638319471f54e083cba4ade0c2a01eb5da
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-05-22 15:46:46 -0400

    🩹  fix export [b]

--- packages/design-system/src/lib/index.ts
+++ packages/design-system/src/lib/index.ts
@@ -1,11 +1,14 @@
 export {
-  Shadows,
-  styled,
-  css,
-  keyframes,
-  getCssText,
-  theme,
   config,
+  createTheme,
+  css,
+  darkTheme,
+  getCssText,
+  globalCss,
+  keyframes,
+  reset,
+  styled,
+  theme,
 } from './stitches.config'
 export { globalStyles } from './globalStyles'
 export type { CSS, VariantProps } from './stitches.config'
