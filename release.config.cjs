58a34a287f46e64d7d153e9b046928a9166f1efb
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-07-02 11:54:08 -0400

    👷  ensure semantic-release-config is built first

--- release.config.cjs
+++ release.config.cjs
@@ -1,41 +1,68 @@
-// /* eslint-disable import/order */
-// const isCI = require('is-ci')
-// !isCI && require('dotenv').config({ path: './.env' })
-// const { getConfig } = require('@jeromefitz/semantic')
+const isCI = require('is-ci')
+!isCI && require('dotenv').config({ path: './.env' })
 const _map = require('lodash/map.js')
 
-const { getConfig } = require('./packages/semantic/dist/index.cjs')
-const releaseBranchTypes = require('./scripts/release-branch-types/index.cjs')
+/**
+ * @note(semantic-release) Conventional Branches
+ *
+ * Configuration object to inform semantic-release what
+ *  the prerelease branches are. Add a key as you see fit,
+ *  and then in the array all acceptable branch names.
+ *
+ * This is done in this manner so that it can be a very
+ *  visual and one-stop-shop of what prerelease branches
+ *  are active on a given repo / branch.
+ *
+ * ex.) feature/cool-feature => feature: ['cool-feature']
+ *
+ */
+const releaseBranchTypes = {
+  ci: [],
+  feature: ['src'],
+  fix: [],
+  refactor: [],
+  release: [],
+}
 
 const branchTypes = _map(
   releaseBranchTypes,
-  (releaseBranchType, releaseBranchTypeIndex) => {
-    return _map(releaseBranchType, (branchType) => {
-      return (
+  (releaseBranchType, releaseBranchTypeIndex) =>
+    _map(
+      releaseBranchType,
+      (branchType) =>
         !!branchType && {
           name: `${releaseBranchTypeIndex}/${branchType}`,
           prerelease: branchType,
         }
-      )
-    })[0]
-  }
+    )[0]
 ).filter((branchType) => !!branchType)
 
 const branches = [
+  // x.y.z
   { name: 'main' },
+  // x.y.z-develop.1|2|3...
   { name: 'canary', prerelease: 'canary' },
+  // x.y.z-develop.1|2|3...
+  { name: 'develop', prerelease: 'develop' },
+  // x.y.z-[branchType].1|2|3...
+  // x.y.z-cool-feature.1|2|3...
   ...branchTypes,
 ]
 
 const config = {
   branches,
-  contributorsProhibitList: {
-    email: [],
-    login: ['BotJerome', 'JeromeFitz'],
-  },
+  extends: ['@jeromefitz/semantic-release-config'],
 }
 
-// const _config = getConfig(config)
+/**
+ * @note(semantic-release) package overrides
+ */
+const getConfig = (overrides) => {
+  return { ...config, ...overrides }
+}
 
+/**
+ * @note(semantic-release) { config, getConfig }
+ */
 module.exports.config = config
 module.exports.getConfig = getConfig
