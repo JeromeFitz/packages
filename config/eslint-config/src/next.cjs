81445d0ca637bbebcfdc846666386899086d034c
Jerome Fitzgerald <j@jeromefitzgerald.com>
2023-01-25 00:24:13 -0500

    ➕  (eslint) tailwindcss, turbo, typescript-sort-keys (#998)
    - [x] Add new packages
    - [x] Remove the tsup stuff for cjs stuff
    - [x] Update the copy scripts
    - [x] Move files to src directory

--- config/eslint-config/src/next.cjs
+++ config/eslint-config/src/next.cjs
@@ -4,9 +4,19 @@ const _extends = [
   ...config.extends,
   'plugin:@next/next/core-web-vitals',
   'plugin:@next/next/recommended',
+  'plugin:turbo',
 ]
 
 module.exports = {
   ...config,
   extends: _extends,
+  rules: {
+    '@next/next/no-html-link-for-pages': 0,
+    'turbo/no-undeclared-env-vars': [
+      'error',
+      {
+        allowList: ['^ENV_[A-Z]+$'],
+      },
+    ],
+  },
 }
