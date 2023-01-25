a9f716a88ad569412dca44c00077fde74e654b8e
Jerome Fitzgerald <j@jeromefitzgerald.com>
2023-01-25 02:01:47 -0500

    🩹  (eslint) rework merge array|object (#998) [b]

--- config/eslint-config/src/react.cjs
+++ config/eslint-config/src/react.cjs
@@ -1,17 +1,13 @@
 const config = require('./typescript.cjs')
 
-const plugins = [...config.plugins, 'jsx-a11y', 'react-hooks']
-
-const rules = {
-  ...config.rules,
-  'react-hooks/exhaustive-deps': 'warn',
-  'react-hooks/rules-of-hooks': 'error',
-}
-
 module.exports = {
   ...config,
-  plugins,
-  rules,
+  plugins: [...config.plugins, 'jsx-a11y', 'react-hooks'],
+  rules: {
+    ...config.rules,
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+  },
   settings: {
     react: {
       version: 'detect',
