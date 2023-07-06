f149eab32dc437dfa3233218aab0738308c20ba5
Jerome Fitzgerald <j@jeromefitzgerald.com>
2023-07-06 12:30:14 -0400

    🩹  (eslint) NICE-54 package upgrades [b] #1132
    💽 lockfile

--- config/eslint-config/release.config.cjs
+++ config/eslint-config/release.config.cjs
@@ -9,7 +9,7 @@ const { name } = require('./package.json')
 
 const branches = [
   ...configDefault.branches,
-  { name: 'feat/config-canary', prerelease: 'canary' },
+  { name: 'NICE-54', prerelease: 'canary' },
 ]
 
 const configPassed = {
