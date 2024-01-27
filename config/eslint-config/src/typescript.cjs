ae73d969bfd838c1cbbe86a2b7619712430fd256
Jerome Fitzgerald <j@jeromefitzgerald.com>
2024-01-27 12:05:27 -0500

    ✨  (eslint) NICE-68 perfectionist [skip ci]

--- config/eslint-config/src/typescript.cjs
+++ config/eslint-config/src/typescript.cjs
@@ -1,40 +1,44 @@
 const config = require('./base.cjs')
 
+const OFF = 0
+const WARN = 1
+// const ERROR = 2
+
 const configTs = {
   overrides: [
     {
-      files: ['**/*.ts?(x)'],
-      parser: '@typescript-eslint/parser',
-      plugins: ['@typescript-eslint', 'import'],
-      parserOptions: {
-        allowAutomaticSingleRunInference: true,
-        ecmaFeatures: {
-          jsx: true,
-        },
-        ecmaVersion: 2022,
-        sourceType: 'module',
-        project: ['./tsconfig.json'],
-        warnOnUnsupportedTypeScriptVersion: true,
-      },
       extends: [
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:import/typescript',
       ],
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        allowAutomaticSingleRunInference: true,
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 2022,
+        project: ['./tsconfig.json'],
+        sourceType: 'module',
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ['@typescript-eslint', 'import'],
       // Set ESLint rule to "never" if TypeScript overrides
       rules: {
-        '@typescript-eslint/explicit-module-boundary-types': 0,
-        '@typescript-eslint/no-empty-function': 0,
-        '@typescript-eslint/no-explicit-any': 0,
-        '@typescript-eslint/no-non-null-assertion': 0,
-        '@typescript-eslint/no-unsafe-argument': 0, // @todo(lint) move to error
-        '@typescript-eslint/no-unsafe-assignment': 0,
-        '@typescript-eslint/no-unsafe-call': 0,
-        '@typescript-eslint/no-unsafe-member-access': 0,
-        '@typescript-eslint/no-unsafe-return': 0,
-        '@typescript-eslint/no-var-requires': 1,
-        '@typescript-eslint/restrict-template-expressions': 0,
+        '@typescript-eslint/explicit-module-boundary-types': OFF,
+        '@typescript-eslint/no-empty-function': OFF,
+        '@typescript-eslint/no-explicit-any': OFF,
+        '@typescript-eslint/no-non-null-assertion': OFF,
+        '@typescript-eslint/no-unsafe-argument': OFF, // @todo(lint) move to error
+        '@typescript-eslint/no-unsafe-assignment': OFF,
+        '@typescript-eslint/no-unsafe-call': OFF,
+        '@typescript-eslint/no-unsafe-member-access': OFF,
+        '@typescript-eslint/no-unsafe-return': OFF,
+        '@typescript-eslint/no-var-requires': WARN,
+        '@typescript-eslint/restrict-template-expressions': OFF,
       },
     },
   ],
